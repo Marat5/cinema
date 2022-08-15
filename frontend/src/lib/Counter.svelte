842997da739b4521f2f7737d45a0956220c05679
Marat Mukhutdinov <mmuhutdinovv@gmail.com>
2022-08-15 16:49:03 +0800

    Playing around with svelte

--- frontend/src/lib/Counter.svelte
+++ frontend/src/lib/Counter.svelte
@@ -1,10 +1,21 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
+  export let count;
+  let doubled = 0;
+  $: {
+    if (count > 10) {
+      break $;
+    }
+
+    doubled = count * 2;
+    console.log(count, doubled);
   }
+
+  const increment = () => {
+    count += 1;
+  };
 </script>
 
+<p>{count} doubled is {doubled}</p>
 <button on:click={increment}>
   count is {count}
 </button>
