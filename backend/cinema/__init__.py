1930cd94830490476596fd3595724ab51049d431
Marat Mukhutdinov <mmuhutdinovv@gmail.com>
2022-10-04 17:34:59 +0300

    Added api endpoints for movies

--- backend/cinema/__init__.py
+++ backend/cinema/__init__.py
@@ -17,7 +17,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'cinema.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'cinema.sqlite')
     )
 
     if test_config is None:
--- backend/cinema/movies.py
+++ backend/cinema/movies.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from cinema.auth import token_required
+from cinema.db import get_db
 
 
 bp = Blueprint("movies", __name__, url_prefix="movies")
@@ -6,9 +9,47 @@ bp = Blueprint("movies", __name__, url_prefix="movies")
 
 @bp.route('/')
 def movies():
-    return jsonify({"movies": [1, 2, 3]})
+    db = get_db()
+    movies = [{"id": item["id"], "title": item["title"], "year": item["year"], "director_id": item["director"], "director": item["name"]}
+              for item in db.execute("SELECT * FROM movie m JOIN director d ON m.director = d.id;").fetchall()]
+    return jsonify({"movies": movies})
 
 
-@bp.route('add')
-def add_movie():
-    return jsonify({"add": [1, 2, 3]})
+@bp.route('add', methods=["POST"])
+@token_required
+def add_movie(current_user):
+    errors = []
+    try:
+        title = request.json['title']
+        director_name = request.json['director']
+        year = request.json['year']
+    except:
+        errors.append("Title and director are required")
+
+    if errors:
+        return jsonify({"errors": errors})
+
+    db = get_db()
+    director = db.execute(
+        "SELECT * FROM director WHERE LOWER(name) LIKE LOWER(?);", (director_name,)).fetchone()
+
+    if not director:
+        db.execute(
+            "INSERT INTO director (name) VALUES (?)", (director_name,))
+        db.commit()
+        director = db.execute(
+            "SELECT * FROM director WHERE LOWER(name) LIKE LOWER(?);", (director_name,)).fetchone()
+
+    db.execute(
+        "INSERT INTO movie (title, added_by, director, year) VALUES (?, ?, ?, ?)", (title, current_user["id"], director["id"], year))
+    db.commit()
+
+    return jsonify({"message": "Success"})
+
+
+@bp.route('directors')
+def directors():
+    db = get_db()
+    directors = [{"id": item["id"], "name": item["name"]}
+                 for item in db.execute("SELECT * FROM director;").fetchall()]
+    return jsonify({"directors": directors})
