72dd33d1b1b4a8c2ba72075ad9e42e84c5652454
Marat Mukhutdinov <mmuhutdinovv@gmail.com>
2022-10-13 23:56:04 +0400

    Splitting the data layer from api method logic

--- backend/cinema/database.py
+++ backend/cinema/database.py
@@ -35,3 +35,48 @@ class Director(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     movies = db.relationship("Movie")
+
+
+class DBHelper():
+    def get_directors(self):
+        return db.session.execute(db.select(Director)).scalars().all()
+
+    def get_director_id(self, director_name):
+        director = db.session.execute(
+            db.select(Director).filter_by(name=director_name)).scalars().first()
+
+        if not director:
+            director = Director(name=director_name)
+            db.session.add(director)
+            db.session.commit()
+
+        return director.id
+
+    def get_movies(self):
+        return db.session.execute(db.select(Movie)).scalars().all()
+
+    def get_movie(self, id):
+        return db.session.execute(
+            db.select(Movie).filter_by(id=id)).scalars().first()
+
+    def update_movie(self, id, title, director_name, year):
+        movie = db_helper.get_movie(id)
+        movie.title = title
+        movie.director = db_helper.get_director_id(director_name)
+        movie.year = year
+
+        db.session.commit()
+        return movie
+
+    def delete_movie(self, id):
+        movie = db_helper.get_movie(id)
+        db.session.delete(movie)
+        db.session.commit()
+
+    def add_movie(self, title, added_by, director_id, year):
+        db.session.add(Movie(title=title, added_by=added_by,
+                             director=director_id, year=year))
+        db.session.commit()
+
+
+db_helper = DBHelper()
--- backend/cinema/movies.py
+++ backend/cinema/movies.py
@@ -1,29 +1,15 @@
 from flask import Blueprint, jsonify, request
 
 from cinema.auth import token_required
-from cinema.database import Director, db, Movie
+from cinema.database import db_helper as dbh
 
 
 bp = Blueprint("movies", __name__, url_prefix="movies")
 
 
-def get_director_id(director_name):
-    director = db.session.execute(
-        db.select(Director).filter_by(name=director_name)).scalars().first()
-
-    if not director:
-        director = Director(name=director_name)
-        db.session.add(director)
-        db.session.commit()
-
-    return director.id
-
-
 @bp.route('/')
 def movies():
-    movies = db.session.execute(db.select(Movie)).scalars().all()
-
-    return jsonify({"movies": movies})
+    return jsonify({"movies": dbh.get_movies()})
 
 
 @bp.route('add', methods=["POST"])
@@ -41,9 +27,8 @@ def add_movie(current_user):
         return jsonify({"errors": errors})
 
     try:
-        db.session.add(Movie(title=title, added_by=current_user.id,
-                             director=get_director_id(director_name), year=year))
-        db.session.commit()
+        dbh.add_movie(title=title, added_by=current_user.id,
+                      director_id=dbh.get_director_id(director_name), year=year)
     except:
         return jsonify({"error": "This movie was already added"})
 
@@ -53,14 +38,12 @@ def add_movie(current_user):
 @bp.route('<movie_id>', methods=["PUT", "DELETE"])
 @token_required
 def update_movie(current_user, movie_id):
-    movie = db.session.execute(
-        db.select(Movie).filter_by(id=movie_id)).scalars().first()
+    movie = dbh.get_movie(movie_id)
     if not movie:
         return jsonify({"error": "Movie with this id does not exist"})
 
     if request.method == "DELETE":
-        db.session.delete(movie)
-        db.session.commit()
+        dbh.delete_movie(movie_id)
         return jsonify({"message": "Success, the movie was deleted"})
 
     try:
@@ -73,16 +56,11 @@ def update_movie(current_user, movie_id):
     if current_user.id != movie.added_by:
         return jsonify({"error": "This movie belongs to different user"})
 
-    movie.title = title
-    movie.director = get_director_id(director_name)
-    movie.year = year
-
-    db.session.commit()
+    movie = dbh.update_movie(movie_id, title, director_name, year)
 
     return jsonify(movie)
 
 
 @bp.route('directors')
 def directors():
-    directors = db.session.execute(db.select(Director)).scalars().all()
-    return jsonify({"directors": directors})
+    return jsonify({"directors": dbh.get_directors()})
