2b969966f8139b6520acc4732e844098285450fe
Marat Mukhutdinov <mmuhutdinovv@gmail.com>
2022-10-15 23:59:56 +0400

    Adjusted token required decorator

--- backend/cinema/auth.py
+++ backend/cinema/auth.py
@@ -14,24 +14,18 @@ bp = Blueprint("auth", __name__, url_prefix="auth")
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = None
-        # jwt is passed in the request header
+        # Check that jwt is passed in the request header
         if 'Authorization' in request.headers and "Bearer " in request.headers['Authorization']:
             token = request.headers['Authorization'].split(" ")[1]
-        # return 401 if token is not passed
         if not token:
             return jsonify({'message': 'Token is missing!'}), 401
 
         try:
-            # decoding the payload to fetch the stored details
+            # Decode the payload to fetch the stored details
             data = jwt.decode(
                 token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
 
             current_user = dbh.get_user(data['id'])
-
-            if current_user is None:
-                raise Exception(
-                    "User with provided jwt doesn't exist (was deleted)")
         except:
             return jsonify({
                 'message': 'Token is invalid!'
@@ -60,9 +54,9 @@ def register():
 
         token = encode_jwt(new_user.id)
     except ValidationError as e:
-        return jsonify({"error": str(e)}), e.code
+        return jsonify({"message": str(e)}), e.code
     except IntegrityError as e:
-        return jsonify({"error": f"User {new_user.username} already exists"}), 409
+        return jsonify({"message": f"User {new_user.username} already exists"}), 409
 
     return jsonify({"token": token})
 
@@ -77,11 +71,11 @@ def login():
         user = dbh.get_user(username=username)
 
         if not check_password_hash(user.password, password):
-            return jsonify({"error": "The password is incorrect"}), 401
+            return jsonify({"message": "The password is incorrect"}), 401
 
         token = encode_jwt(user.id)
     except (ValidationError, ResourceDoesNotExistError) as e:
-        return jsonify({"error": str(e)}), e.code
+        return jsonify({"message": str(e)}), e.code
 
     return jsonify({"token": token})
 
